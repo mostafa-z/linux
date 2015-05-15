d79b27c39dd47d28bbcb99729c31aa397082d6a6
mostafa-z <mostafazarghami@gmail.com>
2015-05-15 21:36:09 +0430

    auto start added-info updated

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 user=$(whoami)
 apt-get install conky conky-all
 cp -p conky.conf conky_no_x11.conf /etc/conky/
+cp -rp conky.desktop /home/$user/.config/autostart/
 cp -pr .fonts/ /home/$user/.fonts/
