34dcafb5097aff01035359a5c6a9367e901a4d88
mostafa-z <mostafazarghami@gmail.com>
2015-05-19 17:09:26 +0430

    add some fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 user=$(whoami)
-apt-get install conky conky-all
+#apt-get install conky conky-all
 cp -p conky.conf conky_no_x11.conf /etc/conky/
 cp -rp conky.desktop /home/$user/.config/autostart/
 cp -pr .fonts/ /home/$user/.fonts/
